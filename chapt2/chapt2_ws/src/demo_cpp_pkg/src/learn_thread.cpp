e04e2541ff655b0c0181bf991621e006b8968de9
OH-MY-GOD-qtl <qiutieling653@gmail.com>
2026-09-16 22:08:25 +0800

    learn_thread_pause

---
+++ chapt2/chapt2_ws/src/demo_cpp_pkg/src/learn_thread.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <functional>
+#include <cpp-httplib/httplib.h>
+
--- chapt2/chapt2_ws/src/demo_python_pkg/setup.py
+++ chapt2/chapt2_ws/src/demo_python_pkg/setup.py
@@ -27,6 +27,7 @@ setup(
         'console_scripts': ['python_node = demo_python_pkg.python_node:main',
                             'person_node = demo_python_pkg.person_node:main',
                             'write_node = demo_python_pkg.write_node:main',
+                            'learn_thread = demo_python_pkg.learn_thread:main',
         ],
     },
 )
