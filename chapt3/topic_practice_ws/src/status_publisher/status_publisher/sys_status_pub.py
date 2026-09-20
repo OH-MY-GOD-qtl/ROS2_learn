3981f3a2fec8c081faa0a8683990ff737726169f
OH-MY-GOD-qtl <qiutieling653@gmail.com>
2026-09-20 20:19:56 +0800

    fix_sys_status

--- chapt3/topic_practice_ws/src/status_publisher/status_publisher/sys_status_pub.py
+++ chapt3/topic_practice_ws/src/status_publisher/status_publisher/sys_status_pub.py
@@ -22,11 +22,11 @@ class SysStatusPub(Node):
         msg.memory_percent = memory_info.percent
         msg.memory_total = memory_info.total / 1024 / 1024
         msg.memory_available = memory_info.available / 1024 / 1024
-        msg.net_sent - net_io_counters.bytes_sent / 1024 / 1024
+        msg.net_sent = net_io_counters.bytes_sent / 1024 / 1024
         msg.net_recv = net_io_counters.bytes_recv / 1024 / 1024
 
         self.get_logger().info(f'发布:{str(msg)}')
-        self.status_publisher_.publisher(msg)
+        self.status_publisher_.publish(msg)
 
 def main():
     rclpy.init()
