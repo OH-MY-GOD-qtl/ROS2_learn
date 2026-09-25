6e2a3a200a532dbff94b9c2d778e2ee818721ecf
OH-MY-GOD-qtl <qiutieling653@gmail.com>
2026-09-25 22:03:51 +0800

    add the picture file path

--- chapt4/chapt4_ws/src/demo_python_service/setup.py
+++ chapt4/chapt4_ws/src/demo_python_service/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + "/resource", ['resource/default.png']),
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
